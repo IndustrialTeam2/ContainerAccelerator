305c87d39f01d5786e1da5539ad049f107ec98b7
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-09-25 16:37:27 +0100

    ec2 instance setup for planning test

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-#Provider and region
-provider aws {
-  region     = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
+  required_version = ">= 1.2.0"
 }
 
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  # Instance name
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
