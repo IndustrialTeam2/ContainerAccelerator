5793352ec6a49bf1ea77f4a54e0cbf0e3673dad2
Zvonimir Delas <zvonimird@dump.hr>
2023-09-25 12:30:28 +0100

    Change region to us-east

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   type        = string
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-east-1"
 }
 
 variable "availability_zones" {
     type = list(string)
     description = "AWS availability zones for the VPC"
-    default = ["us-west-1a", "us-west-1b"]
+    default = ["us-east-1a", "us-east-1b"]
 }
